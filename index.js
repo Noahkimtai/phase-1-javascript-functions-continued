18d133e385e2fdda0e917d0bed0065482bf2a292
Noahkimtai <noahkimtai24@gmail.com>
2022-08-23 11:21:59 +0300

    Completed assignemnt

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+let defActivity = 'roller-skate'
+function saturdayFun(activity){
+    if (activity===undefined){
+        return `This Saturday, I want to ${defActivity}!`
+    }
+    else{return `This Saturday, I want to ${activity}!`}
+}
+let defaultActivity = 'go to the office'
+function mondayWork (activity){
+    if (activity===undefined){
+        return `This Monday, I will ${defaultActivity}.`
+    }
+    else{
+        return `This Monday, I will ${activity}.`
+    }
+
+}
+function wrapAdjective(visual="*"){
+    return function(adj = "special"){
+        return `You are ${visual}${adj}${visual}!`
+    }
+}
